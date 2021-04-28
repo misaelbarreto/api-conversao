98327877bd0547cd8af093d18dff8971e489125e
misaelbarreto <>
2021-04-28 14:07:47 -0300

    * Adicionando comando para registrar imagem no dockerhub.

---
+++ registry.sh
@@ -0,0 +1,4 @@
+# https://hub.docker.com/repository/docker/misaelbarreto/api-conversao
+docker push misaelbarreto/api-conversao:v1
+docker tag misaelbarreto/api-conversao:v1  misaelbarreto/api-conversao:latest
+docker push misaelbarreto/api-conversao:latest
